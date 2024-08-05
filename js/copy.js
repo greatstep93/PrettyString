34b38f168bd1c96747e80a15fc2efeeccf046405
datkachenko <datkachenko@rtech.ru>
2024-08-05 16:40:11 +0400

    add copy for tg

--- js/copy.js
+++ js/copy.js
@@ -6,6 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+  const link = document.getElementById('copy-button-from-tg');
+  // onClick's logic below:
+  link.addEventListener('click', function () {
+    copyToClipboardForTg();
+  });
+});
+
 function copyToClipboard() {
   let text = document.getElementById('input2').innerText;
   const re = new RegExp(String.fromCharCode(160), "g");
@@ -16,4 +24,20 @@ function copyToClipboard() {
   }, function (err) {
     console.error('Произошла ошибка при копировании текста: ', err);
   })
+}
+
+function copyToClipboardForTg() {
+  let text = document.getElementById('input2').innerText;
+  const re = new RegExp(String.fromCharCode(160), "g");
+  text = text.replace(re, ' ');
+  text = '\`\`\`' + text + '\`\`\`'
+  if (text.length <= 4096) {
+    navigator.clipboard.writeText(text).then(function () {
+      console.log('Текст успешно скопирован в буфер обмена');
+    }, function (err) {
+      console.error('Произошла ошибка при копировании текста: ', err);
+    })
+  } else {
+    alert('Максимальная длинна для сообщения для telegram 4096 символов')
+  }
 }
