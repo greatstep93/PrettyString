c0fdfdb848aab48caa2a5e74eb8eef0627ba754e
datkachenko <datkachenko@rtech.ru>
2024-08-05 17:16:56 +0400

    add copy for tg

---
+++ build.sh
@@ -0,0 +1,2 @@
+rm PrettyString.zip
+zip -r PrettyString.zip css images js manifest.json popup.html
--- js/copy.js
+++ js/copy.js
@@ -14,30 +14,44 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+const emptyError = `
+Пустой output.\n
+Сначала нажмите Pretty, что бы преобразовать body.
+`
+
 function copyToClipboard() {
   let text = document.getElementById('input2').innerText;
-  const re = new RegExp(String.fromCharCode(160), "g");
-  text = text.replace(re, ' ');
-  navigator.clipboard.writeText(text)
-  .then(function () {
-    console.log('Текст успешно скопирован в буфер обмена');
-  }, function (err) {
-    console.error('Произошла ошибка при копировании текста: ', err);
-  })
-}
-
-function copyToClipboardForTg() {
-  let text = document.getElementById('input2').innerText;
-  const re = new RegExp(String.fromCharCode(160), "g");
-  text = text.replace(re, ' ');
-  text = '\`\`\`' + text + '\`\`\`'
-  if (text.length <= 4096) {
-    navigator.clipboard.writeText(text).then(function () {
+  if (text.length === 0 || text === emptyError) {
+    document.getElementById("input2").innerHTML = emptyError
+  } else {
+    const re = new RegExp(String.fromCharCode(160), "g");
+    text = text.replace(re, ' ');
+    navigator.clipboard.writeText(text)
+    .then(function () {
       console.log('Текст успешно скопирован в буфер обмена');
     }, function (err) {
       console.error('Произошла ошибка при копировании текста: ', err);
     })
-  } else {
-    alert('Максимальная длинна для сообщения для telegram 4096 символов')
   }
+}
+
+function copyToClipboardForTg() {
+  let text = document.getElementById('input2').innerText;
+  if (text.length === 0 || text === emptyError) {
+    document.getElementById("input2").innerHTML = emptyError
+  } else {
+    const re = new RegExp(String.fromCharCode(160), "g");
+    text = text.replace(re, ' ');
+    text = '\`\`\`' + text + '\`\`\`'
+    if (text.length <= 4096) {
+      navigator.clipboard.writeText(text).then(function () {
+        console.log('Текст успешно скопирован в буфер обмена');
+      }, function (err) {
+        console.error('Произошла ошибка при копировании текста: ', err);
+      })
+    } else {
+      alert('Максимальная длинна для сообщения для telegram 4096 символов')
+    }
+  }
+
 }
